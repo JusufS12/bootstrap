d9671cdca1f89ef5f440338f0ded175d5bcb641f
Josip Sikiric <josip.sikiric@proton.me>
2025-02-14 17:56:00 +0100

    kao nesto

--- main.py
+++ main.py
@@ -2,28 +2,30 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from handleJson import HandleJson
 
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
 
 @app.route('/')
 def index():
     text = 'ovo je početna stranica!'
     return render_template('index.html',text=text)
 
+
 @app.route('/columns_equal_3')
 def columns_equal_3():  
     return render_template('columns_equal_3.html')
+
 
 @app.route('/columns_unequal_2')
 def columns_unequal_2():  
     return render_template('columns_unequal_2.html')
 
+
 @app.route('/columns_unequal_3')
 def columns_unequal_3():  
     return render_template('columns_unequal_3.html')
-
-
-
 
 
 @app.route("/forma", methods=['get', 'post'])
@@ -46,9 +48,6 @@ def forma():
         return redirect(url_for('forma'))
     
     return render_template('form.html', redni_broj=redni_broj, prog_jezici=prog_jezici)
-
-
-
 
 
 # Error pages
